d8bd137f5e0cb3c60bbecb9040ff9acd7c35294f
Fernandogith <fernandosouza.inbox@outlook.com>
2022-11-11 13:35:22 -0300

    dist

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-<<<<<<< HEAD
   baseURL:'http://start.vps-kinghost.net:8000/'
-=======
-  baseURL:'http://start.vps-kinghost.net:8000/'
->>>>>>> origin/dev
 })
 
 export default api;
