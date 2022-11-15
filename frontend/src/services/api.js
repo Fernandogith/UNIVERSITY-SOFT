d63bd109afee495e5947163186e5c58bfb863fe4
Fernandogith <fernandosouza.inbox@outlook.com>
2022-11-15 03:14:20 -0300

    Concluido

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'http://localhost:8000'
-//   baseURL:'https://start.vps-kinghost.net:8000/'
+
+  baseURL:'https://start.vps-kinghost.net:8000/'
 })
 
 
