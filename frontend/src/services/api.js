324590ed490a738715414df656e3d64dc1ddf8e4
Fernandogith <fernandosouza.inbox@outlook.com>
2022-11-15 02:43:44 -0300

    c

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL:'http://localhost:8000'
-//   baseURL:'https://start.vps-kinghost.net:8000/'
+  baseURL:'https://start.vps-kinghost.net:8000/'
 })
 
 
