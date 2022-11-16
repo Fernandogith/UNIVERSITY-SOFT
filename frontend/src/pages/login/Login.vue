986b63c451914ad42d8592ee59f84b341ffef3de
Fernandogith <fernandosouza.inbox@outlook.com>
2022-11-16 16:45:02 -0300

    Responsividade Login

--- frontend/src/pages/login/Login.vue
+++ frontend/src/pages/login/Login.vue
@@ -1,31 +1,52 @@
 <template>
     <main>
-        <header>
-            <div class="container-fluid">
-                <div class="logo">
-                    <a href="/">
-                        <img src="@/assets/img/traco-logo.svg" alt="">
-                        <strong>University</strong> <span>&nbsp;Soft</span> 
-                    </a>
+
+            <header>
+                <div class="container-fluid">
+                    <div class="logo">
+                        <a href="/">
+                            <img src="@/assets/img/traco-logo.svg" alt="">
+                            <strong>University</strong> <span>&nbsp;Soft</span>
+                        </a>
+                    </div>
+                    <!-- Botão menu responsivo -->
+                    <button class="btn-menu-responsivo" @click="abrirMenu">
+                        <img src="@/assets/img/icones/icon-menu-responsivo.svg" alt="">
+                    </button>
+            
+                    <!-- Navegação Desktop -->
+                    <nav class="menu-desktop">
+                        <ul>
+                            <li><a href="/">Inicio</a></li>
+                            <li><a href="/">Quem Somos</a></li>
+                            <li><a href="/">Contate-nos</a></li>
+                        </ul>
+                    </nav>
                 </div>
-                <nav>
+            
+                <!-- Menu Responsivo -->
+                <nav class="menu-responsivo" id="menu-responsivo">
+                    <div class="logo">
+                        <a href="/">
+                            <img src="@/assets/img/traco-logo.svg" alt="">
+                            <strong>University</strong> <span>&nbsp;Soft</span>
+                        </a>
+                    </div>
+            
                     <ul>
-                        <li>
-                            <a href="/">Inicio</a>
-                        </li>
-                        <li>
-                            <a href="/">Quem Somos</a>
-                        </li>
-                        <li>
-                            <a href="/">Contate-nos</a>
-                        </li>
+                        <li><a href="/" @click="fecharMenu">Quem Somos</a></li>
+                        <li><a href="/" @click="fecharMenu">Contate-nos</a></li>
                     </ul>
+                    
                 </nav>
-                <div class="btn-registrar">
-                    <button class="btn-login">Registrar</button>
+            
+                <!-- Overley Responsivo -->
+                <div class="overlay" id="overlay" @click="fecharMenu">
+            
                 </div>
-            </div>
-        </header>
+            
+            </header>
+        
 
         <section class="main">
             <div class="container-fluid">
@@ -65,6 +86,9 @@
                 </div>
             </div>
         </section>
+        <footer>
+            <h2>© Copyright 2022. Developed by Fernando Souza</h2>
+        </footer>
 
     </main>
 </template>
@@ -100,6 +124,24 @@ export default {
                 }
 
             });
+        },
+
+        abrirMenu: function() {
+            const openMenu = document.getElementById("menu-responsivo");
+                openMenu.classList.add("active");
+
+            const openOverlay = document.getElementById("overlay");
+                openOverlay.classList.add("active");
+        
+        },
+
+        fecharMenu: function() {
+            const openMenu = document.getElementById("menu-responsivo");
+                openMenu.classList.remove("active");
+
+            const openOverlay = document.getElementById("overlay");
+                openOverlay.classList.remove("active");
+        
         }
     },
     mounted() {
