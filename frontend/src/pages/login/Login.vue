e23db6401a05b2176502cd7efc32f2be83706db7
Fernandogith <fernandosouza.inbox@outlook.com>
2022-11-18 17:11:04 -0300

    Ajuste botão com problema por estar com onclick

--- frontend/src/pages/login/Login.vue
+++ frontend/src/pages/login/Login.vue
@@ -67,7 +67,7 @@
                         </div>
                         <div class="btn-access">
 
-                            <a class="button" @click="acessar(objDadosAcesso)" onclick="acessar(objDadosAcesso)" >Acessar</a>
+                            <a class="button" @click="acessar(objDadosAcesso)">Acessar</a>
                             
                         </div>
                         <span>━━━━━━━━━━&nbsp;&nbsp;&nbsp;&nbsp;continue por&nbsp;&nbsp;&nbsp;&nbsp;━━━━━━━━━━</span>
@@ -114,18 +114,22 @@ export default {
 
         // Valida o acesso no sistema
         acessar: function (pDadosAcesso) {
+            try {
+                debugger
+                api.post('/login', pDadosAcesso).then(response => {
 
+                    if (response.data == 'Sucesso') {
+                        localStorage.setItem('usuario', pDadosAcesso.usuario);
+                        window.location.href = '/inicio'
+                    } else {
+                        alert('Dados Incorretos, verifique!')
+                    }
 
-            api.post('/login', pDadosAcesso).then(response => {
+                });                
+            } catch (error) {
+                console.log(error);
+            }
 
-                if (response.data == 'Sucesso') {
-                    localStorage.setItem('usuario', pDadosAcesso.usuario);
-                    window.location.href = '/inicio'
-                } else {
-                    alert('Dados Incorretos, verifique!')
-                }
-
-            });
         },
 
         abrirMenu: function() {
